728f8581915b89c2b611e9b56ae85d120b38bd1d
annaguo <kexin.guo@mail.utoronto.ca>
2020-03-03 18:04:25 -0500

    admin done

--- src/views/admin/admin.js
+++ src/views/admin/admin.js
@@ -1,175 +1,111 @@
-// initial list of user
-const course1 = [{
-        course: "CSC309",
-        mute: true
-    },
+//variables
+const students = [
     {
-        course: "STA302",
-        mute: true
-    },
-    {
-        course: "MAT344",
-        mute: false
-    },
-    {
-        course: "MAT315",
-        mute: false
-    }
-]
-
-const course2 = [{
-        course: "CSC309",
-        mute: false
-    },
-    {
-        course: "INI318",
-        mute: false
-    },
-    {
-        course: "CSC373",
-        mute: false
-    },
-    {
-        course: "CSC301",
-        mute: false
-    }
-]
-
-const course3 = [{
-        course: "CSC309",
-        mute: false
-    },
-    {
-        course: "STA247",
-        mute: false
-    },
-    {
-        course: "CSC311",
-        mute: false
-    }
-]
-
-const users = [{
-        username: "Yun1",
-        email: "yun@mail.com",
-        password: "yun1",
-        course: course1
-    },
-    {
-        username: "Yun2",
-        email: "yun2@mail.com",
-        password: "yun2",
-        course: course2
-    },
-    {
-        username: "Yun3",
-        email: "yun3@mail.com",
-        password: "yun3",
-        course: course3
-    }
-];
-
-
-// initialize table body
-const body = users.map(u =>
-    `<tr id=this.rowIndex>
-        <td>${u.username }</td>
-        <td>${u.email}</td>
-        <td>
-            <i id="${u.username}" onclick="allCourses(this.id)" class = "el-icon-notebook-1" data-toggle = "modal" data-target="#muteModal"></i>
-        </td>
-        <td>
-            <i id="edit+${u.username}" class="el-icon-edit" data-toggle="modal" data-target="#editModal"></i>
-        </td>
-        <td>
-            <i id="delete" onclick = "del(this)" class = "el-icon-delete" data-toggle = "modal" data-target="deleteModal"></i>
-        </td>
-    </tr>`);
-
-body.forEach(element => {
-    document.querySelector("#tableBody").innerHTML += element;
-});
-
-// delete a user
-function del(row) {
-  console.log("del(row)");
-    var i = row.parentNode.parentNode;
-    i.parentNode.removeChild(i);
-}
-
-// initial muteCourses modal
-function allCourses(username) {
-  console.log("allCourses("+username+")");
-    let body = "";
-    for (u of users) {
-        if (u.username === username) {
-            for (c of u.course) {
-                if (c.mute) {
-                    body += `<p> ${c.course}
-                                <i id="${username}${c.course}" class="el-icon-turn-off-microphone" onclick="changeStatus('${username}', '${c.course}')">
-                                </i>
-                            </p>`;
-                } else if (!c.mute) {
-                    body += `<p> ${c.course}
-                                <i id="${username}${c.course}" class="el-icon-microphone" alt="nMute" onclick="changeStatus('${username}', '${c.course}')">
-                                </i>
-                            </p>`;
-                }
+        id: 1,
+        userName: "user",
+        email: "user@user.com",
+        pw: "user",
+        courses: [
+            {
+                courseName:'CSC309',
+                isMuted: false
+            },
+            {
+                courseName:"BIO220",
+                isMuted: true
+            },
+            {
+                courseName:"MAT257",
+                isMuted: false
             }
-            document.getElementById("courseBody").innerHTML = body;
-        }
-    }
-}
-
-function changeStatus(username, course) {
-  console.log("changeStatus("+username+", "+ course+")");
-    // change status in user
-    let status;
-    for (u of users) {
-        if (u.username === username) {
-            for (c of u.course) {
-                if (c.course == course) {
-                    c.mute = !c.mute;
-                    status = c.mute;
-                    break;
-                }
+        ]
+    },
+    {
+        id: 2,
+        userName: "user2",
+        email: "user2@user.com",
+        pw: "user2",
+        courses: [
+            {
+                courseName:"CSC309",
+                isMuted: false
+            },
+            {
+                courseName:"BIO220",
+                isMuted: false
+            },
+            {
+                courseName:"MAT257",
+                isMuted: false
             }
-            break;
-        }
+        ]
     }
+]
+//display all the students
+students.map((student) => {
+    const tableEle = $('table')
+    const trEle = '<tr id='+ student.id +'><td class="user-name">' + student.userName + 
+                    '</td><td class="user-email">' + student.email +'</td><td>'+ 
+                    getCoursesEle(student) + '</td><td><ul>' +
+                    '<li><input class="new-name" type="text" placeholder="New Name"><button class="edit-btn save-name-btn">Save</button></li>'+
+                    '<li><input class="new-email" type="text" placeholder="New Email"><button class="edit-btn save-email-btn">Save</button></li>' + 
+                    '<li><input class="new-pw" type="text" placeholder="New Password"><button class="edit-btn save-pw-btn">Save</button></li>'+
+                    '</ul></td></tr>'
+    tableEle.append(trEle)
+})
 
-    // change image
-    let x = document.getElementById(`${username}${c.course}`);
-    console.log(x.src)
-    if (c.mute) {
-        x.alt = "Mute";
-        x.className = "el-icon-turn-off-microphone";
-    } else {
-        x.alt = "nMute";
-        x.className = "el-icon-microphone";
-    }
+function getCoursesEle(student){
+    let result = '<ul>'
+    student.courses.map(course => {
+        const muteBtnText = (course.isMuted ? "unmute" : "mute")
+        const ele = '<li>' + course.courseName + '<button class="mute">' + muteBtnText + '</button></li>';
+        result += ele;
+    })
+    result += '</ul>'
+    return result
 }
 
-// edit user's information
-function saveEdit(e) {
-    // this.id <=> e
-    console.log(e);
-    console.log("!!!!!!!!!!!!!!!!!!")
-        // modified value
-    const newName = document.getElementById("username").value;
-    const newEmail = document.getElementById("email").value;
-    const newPasswd = document.getElementById("pw").value;
-    const body = document.getElementById("tableBody");
 
+$(document).on('click', '.mute', function(e) {
+    //find the student
+    const id = $(e.target).parent().parent().parent().parent().attr('id')
+    const len = (e.target.innerText == 'mute') ? 4 : 6
+    const innerText = $(e.target).parent()[0].innerText
+    const courseName = innerText.slice(0, innerText.length-len)
+    const course = findCourse(id, courseName)
+    const btnText = (course.isMuted) ? "mute" : "unmute"
+    course.isMuted = !course.isMuted;
+    e.target.innerText = btnText
+})
+function findCourse(id, courseName){
+    const student = students.filter(student => student.id == id)[0];
+    return student.courses.filter(course=> course.courseName == courseName)[0]
+}
 
-    const changeHeader = e.parentNode.parentNode.children[0].innerText;
-    let row;
-    for (let i = 0; i <= body.childNodes.length; i++) {
-        console.log(body.childNodes[i].children.textContent);
-        console.log(changeHeader);
-        if (body.childNodes[i].children.textContent == changeHeader)
-            console.log(r);
+$(document).on('click', '.edit-btn', function(e){
+    if($(e.target).hasClass('save-name-btn')){
+        const newName = $(e.target).parent().find('.new-name').val()
+        $(e.target).parent().find('.new-name').val("")
+        const studentID = $(e.target).parent().parent().parent().parent().attr('id')
+        const student = findStudent(studentID)
+        student.userName = newName;
+        $(e.target).parent().parent().parent().parent().find('.user-name')[0].innerText = newName
+
+    }else if($(e.target).hasClass('save-email-btn')){
+        const newEmail = $(e.target).parent().find('.new-email').val()
+        $(e.target).parent().find('.new-email').val("")
+        const studentID = $(e.target).parent().parent().parent().parent().attr('id')
+        const student = findStudent(studentID)
+        student.email = newEmail;
+        $(e.target).parent().parent().parent().parent().find('.user-email')[0].innerText = newEmail
+    }else {
+        const newPW = $(e.target).parent().find('.new-pw').val()
+        $(e.target).parent().find('.new-pw').val("")
+        const studentID = $(e.target).parent().parent().parent().parent().attr('id')
+        const student = findStudent(studentID)
+        student.pw = newPW;
     }
-
-    const changeInfo = e.parentNode.parentNode.children[1]
+})
+function findStudent(id){
+    return students.filter(student => student.id == id)[0]
 }
