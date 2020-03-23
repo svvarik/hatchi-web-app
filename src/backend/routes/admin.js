749210c78f9c2eb057355950fe5a949c137bb73b
yun <Guaun3@users.noreply.github.com>
2020-03-22 23:15:29 -0400

    added admin routes

--- src/backend/routes/admin.js
+++ src/backend/routes/admin.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var model = require('../models');
 
 /* get admin page */
-router.get('/admin', function(request, response, next){
-    response.render('admin', {});
+router.get('/admin', (req, res) => {
+    res.send('admin');
 } )
+
+module.exports = router;
