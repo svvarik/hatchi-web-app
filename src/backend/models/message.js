b1add5beb2ea884cbe167f3bdc47279d2e6a799d
annaguo <kexin.guo@mail.utoronto.ca>
2020-03-26 22:11:53 -0400

    added a dependency, modified the message model

--- src/backend/models/message.js
+++ src/backend/models/message.js
@@ -9,3 +9,25 @@
 // const Message = mongoose.model('Message', messageSchema)
 
 // module.exports = { Message }
+
+/* Message mongoose model */
+const mongoose = require('mongoose')
+const { ObjectID } = require('mongodb')
+
+const Message = mongoose.model('Message', {
+	userID: {
+		type: ObjectID,
+		required: true,
+		minlegth: 1
+	},
+    text: {
+        type: String,
+        required: true
+    },
+    courseID: {
+        type: ObjectID,
+        required: true
+    }
+})
+
+module.exports = { Message }
