60320bdc0fac5c952a12ba4cd76c6acdbde445bb
Sai Vinay Varikooty <sv.varikooty@mail.utoronto.ca>
2020-04-04 21:09:13 -0400

    adding task functionality

--- src/frontend/views/tasks/tasks.js
+++ src/frontend/views/tasks/tasks.js
@@ -1,48 +1,18 @@
 //variables
-const tasks = [
-    {
-        id: "t1",
-        courseCode: "BIO220",
-        taskName: "Infectious Disease Essay",
-        dueDate: new Date(2020, 2, 28, 10),
-        // percentPeopleDone: 0.2,
-        breakdowns: [
-            {partName: "brainstorm", startTime: new Date(2020, 1, 20, 9), endTime: new Date(2020, 1, 20, 11)},
-            {partName: "research", startTime: new Date(2020, 1, 25, 12), endTime: new Date(2020, 1, 25, 3)},
-            {partName: "draft", startTime: new Date(2020, 2, 10, 9), endTime: new Date(2020, 2, 12, 9)},
-            {partName: "final version", startTime: new Date(2020, 2, 22, 20), endTime: new Date(2020, 1, 23, 20)}
-        ]
-    },
-    {
-        id: "t2",
-        courseCode: "CSC309",
-        taskName: "project phase 1",
-        dueDate: new Date(2020, 0, 28, 10),
-        // percentPeopleDone: 0.9,
-        breakdowns: [
-            {partName: "design", startTime: new Date(2020, 1, 25, 12), endTime: new Date(2020, 1, 25, 3)},
-            {partName: "implement", startTime: new Date(2020, 2, 10, 9), endTime: new Date(2020, 2, 12, 9)}
-        ]
-    },
-    {
-        id: "t3",
-        courseCode: "CSC373",
-        taskName: "assignment 2",
-        dueDate: new Date(2020, 2, 28, 10),
-        // percentPeopleDone: 0.1,
-        breakdowns: [
-            {partName: "draft", startTime: new Date(2020, 1, 20, 9), endTime: new Date(2020, 1, 20, 11)},
-            {partName: "discuss", startTime: new Date(2020, 1, 25, 12), endTime: new Date(2020, 1, 25, 3)},
-            {partName: "final version", startTime: new Date(2020, 2, 22, 20), endTime: new Date(2020, 1, 23, 20)}
-        ]
-    }
-    
-]
-let currID = 4;
-const l = console.log
-l(tasks[0].dueDate)
+const currUserID = sessionStorage.getItem('user')
+console.log(currUserID)
 
-//display the tasks
+const getUrl = "https://fast-crag-76695.herokuapp.com/user-tasks/5e7d5a1935577101064fa228"
+
+let tasks;
+
+// fetch(getUrl).then((res) => {
+//     tasks = JSON.parse(res)
+// })
+// let currID = 4;
+// const l = console.log
+
+//display the tasksnpm
 const tasksContainer = $('tasks-content');
 displayAllTasks();
 function displayAllTasks(){
@@ -55,12 +25,11 @@ function displayTask(task){
     const ele = '<div class="card-styling box task-box" id=' + task.id + '>' +
                 '<div class="task-color-bar"></div>' +
                 '<div class="task-box-content">' +
-                '<h2 class="task-name">' + task.courseCode + ": " +
-                task.taskName + '</h2>' +
-                '<h5 class="due-date">Due: ' + task.dueDate.toDateString() +
+                '<h2 class="task-name">' + task.course + ": " +
+                task.title + '</h2>' +
+                '<h5 class="due-date">Due: ' + task.endDate.toDateString() +
                 '</h5><div><img class="edit-btn" src="edit-24px.svg" alt="edit"></div></div></div>';
     $('#add-task-box').before(ele);
-
 }
 
 //display task detail after clicked
@@ -80,13 +49,13 @@ function showTaskDetail(task){
                             '<div class="task-detail card-styling" id=' + task.id + '><div>' + 
                             '<img id="close-btn" src="close-24px.svg" alt="close"></div>' +
                             '<h1>' + task.taskName + '</h1>' +
-                            '<h2>Due: ' + task.dueDate.toDateString() + '</h2></div>';
+                            '<h2>Due: ' + task.endDate.toDateString() + '</h2></div>';
     $('.page-content').append(detailContainer);
     const table = generateHeaderRow();
     $('.task-detail').append(table);
-    l(task.breakdowns)
-    task.breakdowns.map(breakdown => {
-        table.append(generateTableRow(breakdown.partName, breakdown.startTime.toDateString(), breakdown.endTime.toDateString()));
+    l(task.subtasks)
+    task.subtasks.map(breakdown => {
+        table.append(generateTableRow(breakdown.title, breakdown.startDate.toDateString(), breakdown.endDate.toDateString()));
     })
     $('.task-detail').append(`<div id="check-btn-container">
                                 <img id="check-btn" src="check_circle_outline-24px.svg" alt="">
@@ -172,6 +141,7 @@ $(document).on('click', '#add-task-box', function (){
     const inputs = '<div id="add-task-inputs"><div>' +
                     '<h3>Course Code: </h3><input type="text" id="new-task-course">' +
                     '</div><br><div><h3>Task Name: </h3><input type="text" id="new-task-name">' +
+                    '</div><br><div><h3>Weight: </h3><input type="number" min="1" max="5" step="0.01" id="new-task-weight">' +
                     '</div><br><div><h3>Due Date: </h3><input type="date" id="new-task-due-date">' +
                     '</div><br><div><h3>Due Time: </h3><input type="time" id="new-task-due-time">' +
                     '</div></div>';
@@ -185,17 +155,45 @@ $(document).on('click', '#to-breakdown-btn', function(){
     const name = $('#new-task-name').val()
     const dueDate = $('#new-task-due-date').val()
     const dueTime= $('#new-task-due-time').val()
+    const weight = $('#new-task-weight').val()
     const dateToStore = new Date(dueDate.slice(0,4), dueDate.slice(5,7)-1, dueDate.slice(8,10), dueTime.slice(0,2), dueTime.slice(3,5))
-    l(dateToStore)
+
     newTask = {
-        id: "t" + currID.toString(),
-        courseCode: courseCode,
-        taskName: name,
-        dueDate: dateToStore,
-        breakdowns: []
+        "course": courseCode,
+        "title": name,
+        "endDate": dateToStore,
+        "weight": weight,
+        "mark": 0
     }
-    currID += 1;
-    l(dateToStore.toDateString())
+
+    const reqUrl = "http://127.0.0.1:5000/user-tasks/" + "5e7d5a1935577101064fa228"
+
+    const request = new Request(reqUrl, {
+        method: 'post', 
+        body: JSON.stringify(newTask),
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+    });
+
+    fetch(request)
+    .then((res) => { 
+        if (res.status === 200) {
+           return res.json()
+       } else {
+            alert('Could not add new task');
+            $('.page-content').find('.overlay').remove();
+            $('.page-content').find('.add-task').remove();
+       }                
+    })
+    .then((json) => {  // the resolved promise with the JSON body
+        const newTaskId = json.taskId;
+        newTask.displayId = newTaskId
+    }).catch((error) => {
+        console.log(error)
+    })
+    
     //empty the window
     $('#add-task-inputs').empty();
     //add new inputs to window
@@ -216,13 +214,49 @@ $(document).on('click', '#finish-add-btn', function(){
     const startTime = $('#subtask-start-time').val();
     const endDate = $('#subtask-end-date').val();
     const endTime = $('#subtask-end-time').val();
-    const newBreakdown = {
-        partName: breakdownName,
-        startTime: new Date(startDate.slice(0, 4), startDate.slice(5, 7)-1, startDate.slice(8, 10), startTime.slice(0, 2), startTime.slice(3,5)),
-        endTime: new Date(endDate.slice(0, 4), endDate.slice(5, 7)-1, endDate.slice(8, 10), endTime.slice(0, 2), endTime.slice(3,5))
+    
+    const newSubTask = {
+        title: breakdownName,
+        startDate: new Date(startDate.slice(0, 4), startDate.slice(5, 7)-1, startDate.slice(8, 10), startTime.slice(0, 2), startTime.slice(3,5)),
+        endDate: new Date(endDate.slice(0, 4), endDate.slice(5, 7)-1, endDate.slice(8, 10), endTime.slice(0, 2), endTime.slice(3,5))
     }
-    newTask.breakdowns.push(newBreakdown);
-    tasks.push(newTask);
+    
+    // const newBreakdown = {
+    //     partName: breakdownName,
+    //     startTime: new Date(startDate.slice(0, 4), startDate.slice(5, 7)-1, startDate.slice(8, 10), startTime.slice(0, 2), startTime.slice(3,5)),
+    //     endTime: new Date(endDate.slice(0, 4), endDate.slice(5, 7)-1, endDate.slice(8, 10), endTime.slice(0, 2), endTime.slice(3,5))
+    // }
+
+    const subReqUrl = "http://127.0.0.1:5000/user-tasks/" + "5e7d5a1935577101064fa228/" + newTask.id
+
+    const addTask = new Request(subReqUrl, {
+        method: 'post', 
+        body: JSON.stringify(newSubTask),
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+    });
+
+
+    fetch(addTask)
+    .then((res) => { 
+        if (res.status === 200) {
+           return res.json()
+       } else {
+            alert('Could not add new task');
+            $('.page-content').find('.overlay').remove();
+            $('.page-content').find('.add-task').remove();
+       }                
+    })
+    .then((json) => {  // the resolved promise with the JSON body
+        newTask.subtasks = []
+        newTask.subtasks.push(newBreakdown);
+        tasks.push(newTask);
+    }).catch((error) => {
+        console.log(error)
+    })
+
     //remove the window [refactor]
     $('.page-content').find('.overlay').remove();
     $('.page-content').find('.add-task').remove();
@@ -240,12 +274,39 @@ $(document).on('click', '#add-more-subtask-btn', function(){
     const startTime = $('#subtask-start-time').val();
     const endDate = $('#subtask-end-date').val();
     const endTime = $('#subtask-end-time').val();
-    const newBreakdown = {
-        partName: breakdownName,
-        startTime: new Date(startDate.slice(0, 4), startDate.slice(5, 7)-1, startDate.slice(8, 10), startTime.slice(0, 2), startTime.slice(3,5)),
-        endTime: new Date(endDate.slice(0, 4), endDate.slice(5, 7)-1, endDate.slice(8, 10), endTime.slice(0, 2), endTime.slice(3,5))
+
+    const newSubTask = {
+        title: breakdownName,
+        startDate: new Date(startDate.slice(0, 4), startDate.slice(5, 7)-1, startDate.slice(8, 10), startTime.slice(0, 2), startTime.slice(3,5)),
+        endDate: new Date(endDate.slice(0, 4), endDate.slice(5, 7)-1, endDate.slice(8, 10), endTime.slice(0, 2), endTime.slice(3,5))
     }
-    newTask.breakdowns.push(newBreakdown);
+
+    const addTask = new Request(url, {
+        method: 'post', 
+        body: JSON.stringify(newSubTask),
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+    });
+
+
+    fetch(addTask)
+    .then((res) => { 
+        if (res.status === 200) {
+           return res.json()
+       } else {
+            alert('Could not add new task');
+            $('.page-content').find('.overlay').remove();
+            $('.page-content').find('.add-task').remove();
+       }                
+    })
+    .then((json) => {  // the resolved promise with the JSON body
+        newTask.subtasks.push(newSubTask);
+    }).catch((error) => {
+        console.log(error)
+    })
+
     const inputFields = $('#add-task-inputs input');
     for(let i = 0; i < inputFields.length; i++){
         l($(inputFields[i]).val())
